ef44109b6be125d547481fa7a7f94aceebd512d3
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2016-02-14 13:53:40 -0500

    Moved saxon to local directory to prepare for deploy

--- allsaxon.js
+++ allsaxon.js
@@ -1,7 +1,7 @@
 var java = require("java");
 java.options.push("-Djava.awt.headless=true");
 //java.options.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005');
-java.classpath.push("/Users/johncarlson/Downloads/www.web3d.org/x3d/tools/jar/saxon9B.jar");
+java.classpath.push("saxon9B.jar");
 java.classpath.push(".");
 
 function translate(infiles) {
