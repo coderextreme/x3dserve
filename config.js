69bf38363a22478b2c4bfa1459f206b5b89960b9
John Carlson <yottzumm@gmail.com>
2016-07-09 14:37:10 -0400

    Updated code from X3DJSONLD

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-module.exports.examples = "/Users/johncarlson/Downloads/www.web3d.org/x3d/content/examples/";
+module.exports.examples = "/c/Users/coderextreme/Documents/NetBeansProjects/x3d-code/www.web3d.org/x3d/content/examples/";
